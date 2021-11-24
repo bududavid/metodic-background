6fdad041bd2453c9243c385eb4687d106cd3464e
bududavid <93273247+bududavid@users.noreply.github.com>
2021-11-24 20:31:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-$(".burger").click(function(c){$(".burger, ul").toggleClass("active")}),$(".metodic-plus").click(function(c){$(".metodic-plus, .metodic-content").toggleClass("active")});
 
-var w = window.screen.availWidth;
-if(w > 1000){
-let number = document.querySelector('h6')
-window.addEventListener('mousemove', function(e) {
-let x = e.clientX / window.innerWidth;
-let y = e.clientY / window.innerHeight;
-number.style.transform = 'translate(+' + x * 15 + 'px, +' + y * 15 + 'px)';
+$('.burger').click(function(event) {
+    $('.burger, ul').toggleClass('active');
 });
-};
