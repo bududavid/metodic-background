b4347d5d8f576bccdc86c710f84ece1dcc276505
bududavid <93273247+bududavid@users.noreply.github.com>
2021-11-24 20:37:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-
-$('.burger').click(function(event) {
-    $('.burger, ul').toggleClass('active');
-});
+$(".burger").click(function(c){$(".burger, ul").toggleClass("active")}),$(".metodic-plus").click(function(c){$(".metodic-plus, .metodic-content").toggleClass("active")});
